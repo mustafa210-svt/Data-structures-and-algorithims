59a192918ae9ae1ee22e08d260b9efe26ec0f784
mustafa210-svt <sevot78@gmail.com>
2026-05-16 15:24:09 +0100

    Add files via upload

--- bracket matching.py
+++ bracket matching.py
@@ -1,1 +1,28 @@
 expression = input("Please input in an expression: ")
+from Stacks import Stack
+object = Stack(size = 30)
+
+open_brackets = ["(","[","{"]
+close_brackets = [")","]","}"]
+
+for i in expression:
+    print(object.list)
+    if i in open_brackets:
+        object.push(item = i )
+    
+    elif i in close_brackets:
+        b = object.list[-1]
+        b_i = open_brackets.index(b)
+        if close_brackets.index(i) == b_i:
+            object.pop()
+     
+        else:
+            break
+
+if len(object.list) > 0:
+    print("Brackets not matched and expression is not balanced")
+else:
+    print("Brackets sorted and expression is balanced")
+
+
+
